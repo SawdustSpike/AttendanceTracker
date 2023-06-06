4abd39e81a362c76fbc4a8424d9a2549ce86bbd0
Michael Cowell <michaelcodesc.gmail.com>
2023-06-06 11:34:32 -0400

    StartedFrontEnd

---
+++ AttendanceTracker.Tests.Shared/Hidden.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AttendanceTracker.Tests.Shared
+{
+    internal class hidden
+    {
+    }
+}
