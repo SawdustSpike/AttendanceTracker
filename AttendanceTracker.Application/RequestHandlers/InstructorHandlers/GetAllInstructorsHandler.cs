a6fda8c442d690c1cbd33739116354fcc14b5083
Michael Cowell <michaelcodesc.gmail.com>
2023-06-08 23:02:44 -0400

    View Complete

---
+++ AttendanceTracker.Application/RequestHandlers/InstructorHandlers/GetAllInstructorsHandler.cs
@@ -0,0 +1,19 @@
+﻿using AttendanceTracker.Data.DataRequestObjects.InstructorRequests;
+
+
+namespace AttendanceTracker.Application.RequestHandlers.InstructorHandlers
+{
+    public class GetAllInstructorsRequest : IRequest<IEnumerable<Instructor>> { }
+
+    internal class GetAllInstructorsHandler : DataHandler<GetAllInstructorsRequest, IEnumerable<Instructor>>
+    {
+        public GetAllInstructorsHandler(IDataAccess dataAccess) : base(dataAccess) { }
+
+        public override async Task<IEnumerable<Instructor>> HandleRequestAsync(GetAllInstructorsRequest request)
+        {
+            var dto = await _dataAccess.FetchListAsync(new GetAllInstructors());
+
+            return dto.Any() ? dto.Select(_ => _.AsInstructor()) : Enumerable.Empty<Instructor>();
+        }
+    }
+}
---
+++ AttendanceTracker.Data/DataRequestObjects/InstructorRequests/GetAllInstructors.cs
@@ -0,0 +1,7 @@
+﻿namespace AttendanceTracker.Data.DataRequestObjects.InstructorRequests
+{
+    public class GetAllInstructors : Parameterless_DataRequest<Instructor_DTO>
+    {
+        public override string GetSql() => Select.FromTable(TableNames.Instructor);
+    }
+}
